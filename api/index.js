63a0587492010f3f8dac8f4917ff72019b793de9
Huy Duc Anh <hda@gmail.com>
2025-08-30 06:31:49 +0900

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,6 +1,23 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const app = express();
+
+//huyducanh_db_user
+//GpGPUNd9pSHNcmPr
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
